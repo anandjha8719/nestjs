ebb0e207f9f108fa77ee771080525a039ed88760
anandjha8719 <anandjha8719@gmail.com>
2025-03-22 20:42:49 +0530

    setup: swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import 'reflect-metadata';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    bodyParser: true, // Enable built-in body parser with default settings
+    bodyParser: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Document Ingestion')
+    .setDescription('App API endpoints')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
